463f6bbc5c44d2971c953a3987a59c1a2812ff9c
Brandon Chiarito <bchiarito@gmail.com>
2016-07-13 16:57:51 -0400

    finished initial work on multiline upgrade

--- plotter.py
+++ plotter.py
@@ -11,10 +11,7 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option('--cut', metavar='F', type='string', action='store',
-                  default='',
-                  dest='cut',
-                  help='')
+# Variable options
 parser.add_option('--var', metavar='F', type='string', action='store',
                   dest='var',
                   help='')
@@ -22,19 +19,12 @@ parser.add_option('--bins', metavar='F', type='string', action='store',
                   default='100,0,100',
                   dest='binning',
                   help='')
-parser.add_option('--name', metavar='F', type='string', action='store',
-    	    	      default = "plot",
-                  dest='name',
-                  help='name of file when saving')
-parser.add_option('--logy', action='store_true', default=False,
-                  dest='logy',
-                  help='logy sacle on y')
-parser.add_option('--logx', action='store_true', default=False,
-                  dest='logx',
-                  help='logx sacle on x')
-parser.add_option('--scaled', action='store_true', default=False,
-                  dest='scale',
-                  help='scale to integral = 1')
+parser.add_option('--cut', metavar='F', type='string', action='store',
+                  default='',
+                  dest='cut',
+                  help='')
+
+# Input files options
 parser.add_option('--sample', metavar='F', type='string', action='store',
                   dest='sample1',
                   help='')
@@ -50,16 +40,14 @@ parser.add_option('--sample3', metavar='F', type='string', action='store',
                   default='',
                   dest='sample3',
                   help='')
-parser.add_option('--save', action='store_true', default=False,
-                  dest='save',
-                  help='save plot')
-parser.add_option('--title', metavar='F', type='string', action='store',
-	    	          default = '',
-                  dest='title',
-                  help='Title of histogram in canvas')
-parser.add_option('--noplot', action='store_true', default=False,
-                  dest='noplot',
-                  help='Do not plot anything')
+parser.add_option('--sample4', metavar='F', type='string', action='store',
+                  default='',
+                  dest='sample4',
+                  help='')
+parser.add_option('--sample5', metavar='F', type='string', action='store',
+                  default='',
+                  dest='sample5',
+                  help='')
 parser.add_option('--tree', metavar='F', type='string', action='store',
                   dest='treename1',
                   help='full path of TTree object in each file')
@@ -75,10 +63,69 @@ parser.add_option('--tree3', metavar='F', type='string', action='store',
   	      	      default = 'fTree2',
                   dest='treename3',
                   help='full path of TTree object in each file')
+parser.add_option('--tree4', metavar='F', type='string', action='store',
+  	      	      default = 'fTree2',
+                  dest='treename4',
+                  help='full path of TTree object in each file')
+parser.add_option('--tree5', metavar='F', type='string', action='store',
+  	      	      default = 'fTree2',
+                  dest='treename5',
+                  help='full path of TTree object in each file')
+
+# Histogram drawing options
+parser.add_option('--logy', action='store_true', default=False,
+                  dest='logy',
+                  help='logy sacle on y')
+parser.add_option('--logx', action='store_true', default=False,
+                  dest='logx',
+                  help='logx sacle on x')
+parser.add_option('--scaled', action='store_true', default=False,
+                  dest='scale',
+                  help='scale to integral = 1')
+parser.add_option('--lumi', type='float', action='store',
+  	      	      default = -1.0,
+                  dest='lumi',
+                  help='luminosity to scale to, if changed from default value')
+parser.add_option('--title', metavar='F', type='string', action='store',
+	    	          default = '',
+                  dest='title',
+                  help='Title of histogram in canvas')
 parser.add_option('--xaxis', metavar='F', type='string', action='store',
   	      	      default = '',
                   dest='xaxis',
                   help='xaxis label')
+parser.add_option('--noplot', action='store_true', default=False,
+                  dest='noplot',
+                  help='Do not plot anything')
+parser.add_option('--errors', action='store_true', default=False,
+                  dest='errors',
+                  help='calls Sumw2() on resulting histogram')
+parser.add_option('--legoff', action='store_true', default=False,
+                  dest='legoff',
+                  help='Turns off legend')
+parser.add_option('--color', metavar='F', type='string', action='store',
+                  dest='color1',
+                  help='color string (kRed, kTeal, etc)')
+parser.add_option('--color1', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='color1',
+                  help='color string (kRed, kTeal, etc)')
+parser.add_option('--color2', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='color2',
+                  help='color string (kRed, kTeal, etc)')
+parser.add_option('--color3', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='color3',
+                  help='color string (kRed, kTeal, etc)')
+parser.add_option('--color4', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='color4',
+                  help='color string (kRed, kTeal, etc)')
+parser.add_option('--color5', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='color5',
+                  help='color string (kRed, kTeal, etc)')
 parser.add_option('--leg', metavar='F', type='string', action='store',
                   dest='legend1',
                   help='legend entry label')
@@ -89,36 +136,67 @@ parser.add_option('--leg1', metavar='F', type='string', action='store',
 parser.add_option('--leg2', metavar='F', type='string', action='store',
   	      	      default = '',
                   dest='legend2',
-                  help='legend2 entry label')
+                  help='legend entry label')
 parser.add_option('--leg3', metavar='F', type='string', action='store',
   	      	      default = '',
                   dest='legend3',
-                  help='legend3 entry label')
-parser.add_option('--dir', action='store_true', default=False,
-                  dest='dir',
-                  help='treat file option as a directory instead of a single file')
-parser.add_option('--txt', action='store_true', default=False,
-                  dest='txt',
-                  help='treat file option as a .txt file with optional cross sections included')
+                  help='legend entry label')
+parser.add_option('--leg4', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='legend4',
+                  help='legend entry label')
+parser.add_option('--leg5', metavar='F', type='string', action='store',
+  	      	      default = '',
+                  dest='legend5',
+                  help='legend entry label')
+
+# Other options
+parser.add_option('--save', action='store_true', default=False,
+                  dest='save',
+                  help='save plot')
+parser.add_option('--name', metavar='F', type='string', action='store',
+    	    	      default = "plot",
+                  dest='name',
+                  help='name of file when saving')
 parser.add_option('--quiet', action='store_true', default=False,
                   dest='quiet',
                   help='less output')
-parser.add_option('--errors', action='store_true', default=False,
-                  dest='errors',
-                  help='calls Sumw2() on resulting histogram')
-parser.add_option('--legoff', action='store_true', default=False,
-                  dest='legoff',
-                  help='Turns off legend')
 parser.add_option('-n', '--num', type='int', action='store',
                   default=-1,
                   dest='nentries',
                   help='max number of entries to draw from files')
-parser.add_option('--lumi', type='float', action='store',
-  	      	      default = -1.0,
-                  dest='lumi',
-                  help='luminosity to scale to, if changd from default value')
 
 (options, args) = parser.parse_args()
+
+def rootcolor(color_string):
+  if color_string == "Red":
+    return ROOT.kRed
+  elif color_string == "Blue":
+    return ROOT.kBlue
+  elif color_string == "Teal":
+    return ROOT.kTeal
+  elif color_string == "Orange":
+    return ROOT.kOrange
+  elif color_string == "Black":
+    return ROOT.kBlack
+  elif color_string == "Pink":
+    return ROOT.kPink
+  elif color_string == "Cyan":
+    return ROOT.kCyan
+  elif color_string == "Yellow":
+    return ROOT.kYellow
+  elif color_string == "Magenta":
+    return ROOT.kMagenta
+  elif color_string == "Azure":
+    return ROOT.kAzure
+  elif color_string == "Green":
+    return ROOT.kGreen
+  elif color_string == "Spring":
+    return ROOT.kSpring
+  elif color_string == "Gray":
+    return ROOT.kGray
+  elif color_string == "White":
+    return ROOT.kWhite
 
 if not options.quiet:
   print "Begin."
@@ -130,16 +208,36 @@ samples = []
 sample = {}
 sample['path'] = options.sample1
 sample['tree'] = options.treename1
+sample['label'] = options.legend1
+sample['color'] = rootcolor(options.color1)
 samples.append(sample)
 if not options.sample2 == "":
   sample = {}
   sample['path'] = options.sample2
   sample['tree'] = options.treename2
+  sample['label'] = options.legend2
+  sample['color'] = rootcolor(options.color2)
   samples.append(sample)
 if not options.sample3 == "":
   sample = {}
   sample['path'] = options.sample3
   sample['tree'] = options.treename3
+  sample['label'] = options.legend3
+  sample['color'] = rootcolor(options.color3)
+  samples.append(sample)
+if not options.sample4 == "":
+  sample = {}
+  sample['path'] = options.sample4
+  sample['tree'] = options.treename4
+  sample['label'] = options.legend4
+  sample['color'] = rootcolor(options.color4)
+  samples.append(sample)
+if not options.sample5 == "":
+  sample = {}
+  sample['path'] = options.sample5
+  sample['tree'] = options.treename5
+  sample['label'] = options.legend5
+  sample['color'] = rootcolor(options.color5)
   samples.append(sample)
 
 # Make collection of TChains
@@ -149,13 +247,13 @@ for sample in samples:
   ISdirectory = False
   ISinputfile = False
   if fnmatch.fnmatch(path, "*.root"):
-    print "I think", path, "is a rootfile"
+    print "It appears ", path, "is a rootfile"
     ISrootfile = True
   if fnmatch.fnmatch(path, "*/"):
-    print "I think", path, "is a directory to traverse"
+    print "It appears", path, "is a directory to traverse"
     ISdirectory = True
   if fnmatch.fnmatch(path, "*.txt"):
-    print "I think", path, "is a text file of inputs"
+    print "It appears", path, "is a text file of inputs"
     ISinputfile = True
 
   if ISrootfile:
@@ -166,7 +264,7 @@ for sample in samples:
   if ISdirectory:
     chain = ROOT.TChain(sample['tree'])
     rootfiles = []
-    for root, dirnames, filenames in os.walk(options.file1):
+    for root, dirnames, filenames in os.walk(path):
       for filename in fnmatch.filter(filenames, '*.root'):
         rootfiles.append(os.path.join(root, filename))
     for rootfile in rootfiles:
@@ -216,7 +314,10 @@ low = float(options.binning[i1+1:i2])
 high = float(options.binning[i2+1:len(options.binning)])
 
 count = 0
+sumcount = 0
 for sample in samples:
+  sumcount += 1
+  hist_sum = ROOT.TH1F(options.name+"_sum_"+str(sumcount), options.name, bins, low, high)
   for entry in sample['entries']:
     chain = entry[0]
     xs = entry[1]
@@ -224,132 +325,103 @@ for sample in samples:
     count += 1
     hist = ROOT.TH1F(options.name+"_"+str(count), options.name, bins, low, high)
     chain.Draw(options.var+">>"+options.name+"_"+str(count),""+options.cut, "goff")
-    entry.append(hist)    
+    entry.append(hist)
+    hist.Scale(xs/N)
+    hist_sum = hist_sum + hist
+  sample['summed_hist'] = hist_sum
   
-  
-    
-
-    # here we should add up all the constitent histograms for a particular sample,
-    # and also scale them against each other
-    # if lumi is set, scale them according to the lumi
-    # if lumi is set but the sample has no xs and N listed, just leave it alone, arbitrary units
-    # if lumi is not set, scale the sample against the sample with the median event content
-
-
-
 
 # Print Summary
-print "\nEntries    : ", int(newhist.GetEntries())
-if not int(newhist.GetBinContent(0)) == 0:
-  print "Underflow  - ", int(newhist.GetBinContent(0))
-if not int(newhist.GetBinContent(bins+1)) == 0:
-  print "Overflow   - ", int(newhist.GetBinContent(bins+1))
-
-if not options.file2 == "":
-  print "Entries 2  : ", int(newhist2.GetEntries())
-  if not int(newhist2.GetBinContent(0)) == 0:
-    print "Underflow 2- ", int(newhist2.GetBinContent(0))
-  if not int(newhist2.GetBinContent(bins+1)) == 0:
-    print "Overflow 2 - ", int(newhist2.GetBinContent(bins+1))
-
-if not options.file3 == "":
-  print "Entries 3  : ", int(newhist3.GetEntries())
-  if not int(newhist3.GetBinContent(0)) == 0:
-    print "Underflow 3- ", int(newhist3.GetBinContent(0))
-  if not int(newhist3.GetBinContent(bins+1)) == 0:
-    print "Overflow 3 - ", int(newhist3.GetBinContent(bins+1))
-print ""
-
+count = 1
+for sample in samples:
+  hist = sample['summed_hist']
+  print "\nEntries " + str(count) + "   : ", int(hist.GetEntries())
+  if not int(hist.GetBinContent(0)) == 0:
+    print "Underflow " + str(count) + " - ", int(hist.GetBinContent(0))
+  if not int(hist.GetBinContent(bins+1)) == 0:
+    print "Overflow " + str(count) + "  - ", int(hist.GetBinContent(bins+1))
+  count += 1
+  
 if not options.noplot:
   if not options.quiet:
     print "Plotting..."
   c = TCanvas()
   c.cd()
-  # Stats
-  newhist.SetLineColor(ROOT.kRed)
-  newhist.SetFillColor(0)
-  newhist.SetLineWidth(2)
-  newhist.SetLineStyle(1)	
-  newhist.SetStats(0)
-  if not options.file2 == "":
-    newhist2.SetLineColor(ROOT.kBlack)
-    newhist2.SetFillColor(0)
-    newhist2.SetLineWidth(2)
-    newhist2.SetLineStyle(1)	
-    newhist2.SetStats(0)
-  if not options.file3 == "":
-    newhist3.SetLineColor(ROOT.kTeal)
-    newhist3.SetFillColor(0)
-    newhist3.SetLineWidth(2)
-    newhist3.SetLineStyle(1)	
-    newhist3.SetStats(0)
-  # Scale
-  if options.errors:
-    newhist.Sumw2()
-    if not options.file2 == "":
-      newhist2.Sumw2()
-    if not options.file3 == "":
-      newhist3.Sumw2()
-  if options.scale:
-    newhist.Scale(100.0/newhist.Integral())
-    if not options.file2 == "":
-      newhist2.Scale(100.0/newhist2.Integral())
-    if not options.file3 == "":
-      newhist3.Scale(100.0/newhist3.Integral())
   if options.logy:
     c.SetLogy()
   if options.logx:
     c.SetLogx()
-  maximum = newhist.GetMaximum()
-  if not options.file2 == "":
-    maximum = max(maximum, newhist2.GetMaximum())
-  if not options.file2 == "":
-    maximum = max(maximum, newhist3.GetMaximum())
-  if options.logy:
-    newhist.SetMaximum(maximum*4)
-    if not options.scale:
-      newhist.SetMinimum(0.5)
-  else:
-    newhist.SetMaximum(maximum*1.15)
+  colorcount = 1
+  for sample in samples:
+    hist = sample['summed_hist']
+    # Stats
+    if sample['color'] == "":
+      hist.SetLineColor(colorcount)
+      colorcount+=1
+    else:
+      hist.SetLineColor(sample['color'])
+    hist.SetFillColor(0)
+    hist.SetLineWidth(2)
+    hist.SetLineStyle(1)
+    hist.SetStats(0)
+    # Scale
+    if options.errors:
+      hist.Sumw2()
+    if options.scale:
+      hist.Scale(100.0/hist.Integral())
+
+  # Maximum
+  maximum = 0
+  for sample in samples:
+    hist = sample['summed_hist']
+    maximum = max(maximum, hist.GetMaximum())
+  for sample in samples:
+    hist = sample['summed_hist']
+    if options.logy:
+      hist.SetMaximum(maximum*4)
+      if not options.scale:
+        # set min if log plot and not scaled
+        hist.SetMinimum(0.5)
+    else:
+      hist.SetMaximum(maximum*1.15)
+
   # Title
-  if not options.title == "":
-    newhist.SetTitle(options.title)
-  elif options.name == "plot":
-    newhist.SetTitle(options.var)
-  else:
-    newhist.SetTitle(options.name)
-  # X axis
-  newhist.GetXaxis().SetTitle(options.var + " w/ " + options.cut)
-  if options.cut == "":
-    newhist.GetXaxis().SetTitle(options.var)
-  if not options.xaxis == "":
-    newhist.GetXaxis().SetTitle(options.xaxis)
-  # Y axis
-  newhist.GetYaxis().SetTitle("Events")
-  if options.scale:
-    newhist.GetYaxis().SetTitle("Scaled to Integral 100")
+  for sample in samples:
+    hist = sample['summed_hist']
+    if not options.title == "":
+      hist.SetTitle(options.title)
+    elif options.name == "plot":
+      hist.SetTitle(options.var)
+    else:
+      hist.SetTitle(options.name)
+    # X axis
+    hist.GetXaxis().SetTitle(options.var + " w/ " + options.cut)
+    if options.cut == "":
+      hist.GetXaxis().SetTitle(options.var)
+    if not options.xaxis == "":
+      hist.GetXaxis().SetTitle(options.xaxis)
+    # Y axis
+    hist.GetYaxis().SetTitle("Events")
+    if options.scale:
+      hist.GetYaxis().SetTitle("Scaled to Integral 100")
+
   # Draw()
-  newhist.Draw()
-  if not options.file2 == "":
-    newhist2.Draw("same")
-  if not options.file3 == "":
-    newhist3.Draw("same")
+  count = 0
+  for sample in samples:
+    hist = sample['summed_hist']
+    if count == 0:
+      hist.Draw()
+    else:
+      hist.Draw("same")
+
   # Legend
   leg = ROOT.TLegend(0.55, 0.80, 0.9, 0.9)
-  leg1 = options.legend1
-  if leg1 == "":
-    leg1 = options.file1
-  leg.AddEntry(newhist, leg1, "l")
-  if not options.file2 == "":
-    leg2 = options.legend2
-    if leg2 == "":
-      leg2 = options.file2
-    leg.AddEntry(newhist2, leg2, "l")
-  if not options.file3 == "":
-    leg3 = options.legend3
-    if leg3 == "":
-      leg3 = options.file3
-    leg.AddEntry(newhist3, leg3, "l")
+  for sample in samples:
+    hist = sample['summed_hist']
+    if sample['label'] == "":
+      leg.AddEntry(hist, sample['path'], "l")
+    else:
+      leg.AddEntry(hist, sample['label'], "l")  
   if not options.legoff:
     leg.Draw("same")
   # Save
@@ -361,6 +433,7 @@ if not options.quiet:
 
 # After plot Commands
 if (not options.save) and (not options.noplot):
+  hist = samples[0]['summed_hist']
   cmd = "start"
   while not cmd == "":
     print "\nPress [Enter] to finish, type 'options' to see options"
@@ -390,20 +463,20 @@ if (not options.save) and (not options.noplot):
       else:
         c.SaveAs(opt + ".png")
     elif cmd == "gaus":
-      newhist.Fit("gaus")
-      newhist.Draw("same")
+      hist.Fit("gaus")
+      hist.Draw("same")
     elif cmd == "fit":
       if opt=="":
         print "Must supply second argument to", cmd
         continue
-      newhist.Fit(opt)
-      newhist.Draw("same")
+      hist.Fit(opt)
+      hist.Draw("same")
     elif cmd == "vertical":
       if opt=="":
         print "Must supply second argument to", cmd
         continue
       pos = float(opt)
-      vert_line = ROOT.TLine(pos, 0, pos, newhist.GetMaximum())
+      vert_line = ROOT.TLine(pos, 0, pos, hist.GetMaximum())
       vert_line.Draw("same")
       if not options.legoff:
         leg.Draw("same")
