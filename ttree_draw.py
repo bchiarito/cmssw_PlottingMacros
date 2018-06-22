47ab106b9ed4cb514f38b42b1568e57ef64a9d11
Brandon Chiarito <bchiarito@gmail.com>
2018-06-22 12:41:22 -0400

    fixed example plotter bug

--- ttree_draw.py
+++ ttree_draw.py
@@ -13,10 +13,10 @@ time_begin = time.time()
 usage = "%prog [options] sample -v VAR -b BINNING -c CUT."
 parser = OptionParser(usage=usage)
 parser.add_option('-v', '--var', '--varx', type='string', action='store', dest='var', help='')
-parser.add_option('-b', '--bin', '--bins', '--binsx', '--binx', type='string', metavar='NUM,LOW,HIGH', action='store', default='100,0,100', dest='binning', help='')
+parser.add_option('-b', '--bin', '--bins', type='string', metavar='NUM,LOW,HIGH', action='store', default='100,0,100', dest='binning', help='')
 parser.add_option('-c', '--cut', type='string', action='store', default='', dest='cut', metavar='CUT_STRING', help='')
 parser.add_option('--tree', type='string', action='store', dest='treename', default='diphotonAnalyzer/fTree2', metavar='PATH', help='path to tree in rootfiles')
-parser.add_option('--mcweight', action='store_true', default=False, dest='mcweight', help='weight entries by XS and Ngen')
+parser.add_option('--mcweight', action='store_true', default=False, dest='mcweight', help='weight entries by XS and Ngen found in ttree as mcXS and mcN')
 parser.add_option('--lumi', type='float', action='store', dest='lumi', default=1.0, help='integrated luminosity to scale to (pb^1)')
 (options, args) = parser.parse_args()
 sys.argv = [] # prevents ROOT from also reading options
@@ -49,11 +49,10 @@ high = float(options.binning[i2+1:len(options.binning)])
 hist = ROOT.TH1F("hist", "hist", bins, low, high)
 
 lumi = options.lumi
-draw_string = options.var + ">>"+"hist"+"_"+str(count)
+draw_string = options.var + ">>"+"hist"
 cut_string = "1" if (options.cut=="" or options.cut==None) else options.cut
 if options.mcweight:
   cut_string = "("+cut_string+")*(mcXS*"+str(lumi)+"/mcN)"
-print draw_string, cut_string
 chain.Draw(draw_string, cut_string, "goff")
 
 # draw the histogram
@@ -64,4 +63,4 @@ hist.Draw()
 # hold until user input
 time_end = time.time()
 print "Elapsed Time: ", "%.1f" % (time_end - time_begin), "sec"
-raw_input()
+raw_input("Press Enter to finish")
