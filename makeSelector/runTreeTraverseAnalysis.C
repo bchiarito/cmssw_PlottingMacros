62f1fe3a9fbaceb21d7a77bbd1f96055125f5ee7
Brandon Chiarito <bchiarito@gmail.com>
2018-11-14 19:43:50 -0500

    big cleanup, removed obsolte code, moved some scripts to /bin which pertained not to plotting

--- makeSelector/runTreeTraverseAnalysis.C
+++
@@ -1,8 +0,0 @@
-{
-  TChain chain("diphotonAnalyzer/fTree2");
-
-  chain.Add("TwoProngNtuplizer_signal125_track10_photon10.root");
-  chain.Add("TwoProngNtuplizer_signal125_track10_photon1.root");
-
-  chain.Process("treeTraverseAnalysis.C")
-}
